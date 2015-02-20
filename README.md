cdfc6ab0b7787911fe23032620cd75109df1f307
Vadim <detriment.ehl@gmail.com>
2015-02-20 05:10:50 -0500

    Initial project version' '

---
+++ README.md
@@ -0,0 +1,3 @@
+Wababa: A Console Question Game
+
+There are 3 questions, each can be answered by opening individual files that will contain the answer.
---
+++ main.cpp
@@ -0,0 +1,15 @@
+/************
+
+Wababa: Simple Question Game
+
+************/
+
+#include <iostream>
+
+int main () {
+
+    // Wababa Welcome.
+    std::cout << "Wababa!\n"
+    // Returns 0
+    return 0;
+}
